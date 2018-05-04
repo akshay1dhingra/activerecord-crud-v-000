a33abc5f4b9f4dafd3c2e8795debc02281fe7df4
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-05-04 21:45:07 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ def can_be_created_with_a_hash_of_attributes
   movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = { title: "Avengers", release_date: 2020 })
   Movie.create do |m|
     __
   end
