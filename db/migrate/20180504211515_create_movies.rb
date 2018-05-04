52be79a144e91163f3d70a6389cc369d95548107
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-05-04 21:17:30 +0000

    automatically backed up by learn

--- db/migrate/20180504211515_create_movies.rb
+++ db/migrate/20180504211515_create_movies.rb
@@ -1,9 +1,9 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.string :title 
+      t.string :title
       t.datetime :release_date
-      t.string :director 
+      t.string :director
       t.string :lead
       t.boolean :in_theaters
     end
