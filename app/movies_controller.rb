d280032c216da71d63290e0e03b555555cf011a3
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-05-04 21:51:37 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,15 +34,15 @@ def can_be_created_in_a_block(args = { title: "Avengers", release_date: 2020 })
 end
 
 def can_get_the_first_item_in_the_database
-  movie.first.title
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last.title
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
